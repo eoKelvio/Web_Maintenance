1385b94e0aa91a2c31f01e3dd3b7b12d693b4c1e
lucasgearhead <lucasdiascsilva@hotmail.com>
2024-09-13 17:10:40 -0300

    feat: create layout file to orchestrate tabs screens

---
+++ mobile_front/app/(tabs)/_layout.tsx
@@ -0,0 +1,88 @@
+import { Octicons, Ionicons } from "@expo/vector-icons";
+import { router, Tabs } from "expo-router";
+import { TouchableOpacity } from "react-native";
+import { ThemeToggle } from "~/components/ThemeToggle";
+import { useColorScheme } from "nativewind";
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      safeAreaInsets={{ bottom: 10 }}
+      screenOptions={{
+        tabBarHideOnKeyboard: true,
+        tabBarShowLabel: false,
+        headerShown: false,
+        headerShadowVisible: false,
+        headerTitleAlign: "center",
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Octicons size={28} name="graph" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="machines"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Octicons size={28} name="apps" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="maintenance"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Octicons size={28} name="tools" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="teams"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Octicons size={28} name="people" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="stock"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Octicons size={28} name="package" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          headerShown: true,
+          title: "User",
+          headerRight: () => <ThemeToggle />,
+          headerLeft: () => (
+            <TouchableOpacity
+              onPress={() => {
+                router.push("/login");
+              }}
+              className="pl-8"
+            >
+              <Ionicons
+                name="log-out-outline"
+                size={28}
+                color={
+                  useColorScheme().colorScheme === "dark" ? "white" : "black"
+                }
+              />
+            </TouchableOpacity>
+          ),
+          tabBarIcon: ({ color }) => (
+            <Octicons size={28} name="gear" color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
