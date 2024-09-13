bff5f91fe65aaaa0633778d2c4c56865531743fe
lucasgearhead <lucasdiascsilva@hotmail.com>
2024-09-13 17:09:02 -0300

    feat: create simple screens to run how example

---
+++ mobile_front/app/(tabs)/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Home() {
+  return (
+    <View className="h-full justify-center bg-secondary/30">
+      <Text className="text-secondary-foreground self-center">HomeScreen</Text>
+    </View>
+  );
+}
