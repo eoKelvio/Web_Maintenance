e06430074f3e355ce973f02710d63a52a2ba54ad
lucasgearhead <jeffersondlsilva@hotmail.com>
2024-09-22 23:43:52 -0300

    feat: create dashboard screen

--- mobile_front/app/(tabs)/index.tsx
+++ mobile_front/app/(tabs)/index.tsx
@@ -5,10 +5,7 @@ import { Separator } from "~/components/ui/separator";
 
 export default function Home() {
   return (
-    <View className="h-screen bg-secondary/30">
-      <Text className="self-center text-2xl font-bold text-primary py-5 pt-20">
-        RECARO
-      </Text>
+    <View className="h-full bg-secondary/30">
       <DashCards />
       <Separator
         orientation="horizontal"
