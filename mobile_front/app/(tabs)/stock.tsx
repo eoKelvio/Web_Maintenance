b6540246e5f57017fc63e072a46be2f981790414
lucasgearhead <jeffersondlsilva@hotmail.com>
2024-09-22 23:42:45 -0300

    feat: created stock screen

--- mobile_front/app/(tabs)/stock.tsx
+++ mobile_front/app/(tabs)/stock.tsx
@@ -1,9 +1,58 @@
-import { View, Text } from "react-native";
+import React from "react";
+import { View, ScrollView } from "react-native";
+import { Text } from "~/components/ui/text";
+import { Card, CardHeader, CardTitle, CardContent } from "~/components/ui/card";
+import { generateRandomStock } from "~/data/mock_items";
+import { useColorScheme } from "~/lib/useColorScheme";
 
 export default function StockScreen() {
+  const itemsWithStock = generateRandomStock();
+  const { isDarkColorScheme } = useColorScheme();
+
+  const getStatusColor = (status: string) => {
+    if (status === "Normal") {
+      return isDarkColorScheme ? "lightgreen" : "green";
+    } else if (status === "Abaixo") {
+      return isDarkColorScheme ? "lightcoral" : "red";
+    } else {
+      return "white";
+    }
+  };
+
   return (
-    <View className="h-full justify-center bg-secondary/30">
-      <Text className="text-primary self-center">Stock</Text>
-    </View>
+    <ScrollView
+      showsVerticalScrollIndicator={false}
+      contentContainerClassName="gap-3 p-3"
+      className="bg-secondary/30 gap-10"
+    >
+      {itemsWithStock.map((item) => (
+        <Card key={item.id} className="bg-secondary/30 -bg-card">
+          <CardHeader className="flex-row justify-between">
+            <CardTitle>{item.name}</CardTitle>
+            <Text
+              style={{
+                color: getStatusColor(item.status),
+              }}
+            >
+              {item.status}
+            </Text>
+          </CardHeader>
+          <CardContent>
+            <View className="justify-between flex-row">
+              <Text>Quantidade Atual: </Text>
+              <Text className="font-bold">
+                {item.currentStock + " " + item.unit}
+              </Text>
+            </View>
+            <View className="justify-between flex-row">
+              <Text>Quantidade Mínima: </Text>
+              <Text className="font-bold">
+                {item.minimumStock + " " + item.unit}
+              </Text>
+            </View>
+          </CardContent>
+        </Card>
+      ))}
+    </ScrollView>
   );
 }
