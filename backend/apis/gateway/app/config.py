761bbb60b4e647d2f320fe55b2aef14a7b9690a1
SamuelFSNunes <samucasantana2003@gmail.com>
2024-12-10 20:01:58 -0300

    feat: able cors headers

--- backend/apis/gateway/app/config.py
+++ backend/apis/gateway/app/config.py
@@ -1,3 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Configurar os domínios permitidos
+origins = [
+    "http://localhost:3000",  # Origem do seu frontend
+    "http://127.0.0.1:3000",  # Origem alternativa do frontend
+    # Adicione outros domínios permitidos, se necessário
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Permitir estas origens
+    allow_credentials=True,  # Permitir envio de cookies
+    allow_methods=["*"],  # Permitir todos os métodos HTTP (GET, POST, etc.)
+    allow_headers=["*"],  # Permitir todos os cabeçalhos
+)
