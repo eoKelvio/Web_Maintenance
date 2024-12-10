c9bd0db8d087a09f90275e2946cfab282e2fea99
lucasgearhead <lucasdiascsilva@hotmail.com>
2024-12-10 20:48:00 -0300

    feat: create url variable to export

--- mobile_front/lib/utils.ts
+++ mobile_front/lib/utils.ts
@@ -1,6 +1,8 @@
-import { clsx, type ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const url = "http://10.109.3.89:9999";
