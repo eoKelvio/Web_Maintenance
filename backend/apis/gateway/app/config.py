54f2949512a7b4a92f36af98130fb0df250f89ed
SamuelFSNunes <samucasantana2003@gmail.com>
2024-12-11 14:46:29 -0300

    buid: add new up in cors headers

--- backend/apis/gateway/app/config.py
+++ backend/apis/gateway/app/config.py
@@ -8,6 +8,7 @@ origins = [
     "http://localhost:3000",
     "http://localhost:8081",   # Origem do seu frontend
     "http://127.0.0.1:3000",  # Origem alternativa do frontend
+    "http://10.109.3.145:8081",
     # Adicione outros domínios permitidos, se necessário
 ]
 
