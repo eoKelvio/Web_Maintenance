ccd0c5dfa720fb56de5b05617a81ec9f63ce8ff3
SamuelFSNunes <samucasantana2003@gmail.com>
2024-12-12 16:00:53 -0300

    feat: implemented getUsers

--- web_front/src/app/machines/page.tsx
+++ web_front/src/app/machines/page.tsx
@@ -4,42 +4,17 @@ import Line from "@/components/line"
 import MachineRegister from "@/components/machine/machine-register"
 import { Machine, columns } from "./columns"
 import { DataTable } from "./data-table"
+import { getMachines } from "@/services/MachineService"
 
 // Função simulada para obter dados de máquinas
-async function getData(): Promise<Machine[]> {
-  return [
-    {
-      id: "123abc",
-      name: "Maquina A",
-      type: "Tipo 1",
-      model: "Modelo X",
-      serialNumber: "SN123456",
-      manufacturingDate: "2020-01-15",
-      location: "Planta 1",
-      maintenanceHistory: ["Manutenção em 2021", "Manutenção em 2022"],
-    },
-    {
-      id: "456def",
-      name: "Maquina B",
-      type: "Tipo 2",
-      model: "Modelo Y",
-      serialNumber: "SN789101",
-      manufacturingDate: "2019-05-20",
-      location: "Planta 2",
-      maintenanceHistory: ["Manutenção em 2020", "Manutenção em 2023"],
-    },
-    {
-      id: "789ghi",
-      name: "Maquina C",
-      type: "Tipo 3",
-      model: "Modelo Z",
-      serialNumber: "SN112233",
-      manufacturingDate: "2018-03-10",
-      location: "Planta 3",
-      maintenanceHistory: ["Manutenção em 2019", "Manutenção em 2021"],
-    },
-  ]
-}
+const getData = async () => {
+  try {
+    const response = await getMachines();
+    return response;
+  } catch (error: any) {
+    console.error("Erro ao coletar(s) o usuário(s):", error.message);
+  }
+};
 
 export default async function Home() {
   const data = await getData()
