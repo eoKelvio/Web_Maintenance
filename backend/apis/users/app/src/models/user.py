0c2ddbe191e91949b0a9c17ed366d1f416469589
MatheusPCV <matheus.p.vieiraa@gmail.com>
2024-12-13 14:58:50 -0300

    docs: add documentation to the users models

--- backend/apis/users/app/src/models/user.py
+++ backend/apis/users/app/src/models/user.py
@@ -5,6 +5,18 @@ from ..database import Base
 
 
 class UserModels(Base):
+    """
+    Represents a user entity in the database.
+
+    Attributes:
+        id (int): The primary key of the user.
+        name (str): The name of the user.
+        username (str): The unique username of the user.
+        password (str): The hashed password of the user.
+        role (str): The role of the user (e.g., admin, team member).
+        team_id (int): The ID of the team the user belongs to.
+    """
+    
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
